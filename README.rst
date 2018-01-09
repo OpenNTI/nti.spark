cd00e7c9dd3cf2dc0ce7f24a967ba5d2444c9195
Carlos Sanchez <papachoco@gmail.com>
2018-01-09 11:22:22 -0600

    inintial check-in

---
+++ README.rst
@@ -0,0 +1,9 @@
+===================
+ nti.spark
+===================
+
+.. image:: https://travis-ci.org/NextThought/nti.spark.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.spark
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.spark/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.spark?branch=master
---
+++ setup.py
@@ -0,0 +1,72 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'fudge',
+    'nti.testing',
+    'zope.dottedname',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.spark',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Spark",
+    long_description=(
+        _read('README.rst') 
+        + '\n\n' 
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='spark',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.orgsync_spark",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'pyspark',
+        'zope.component',
+        'zope.interface',
+        'zope.schema',
+        'zope.security',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
