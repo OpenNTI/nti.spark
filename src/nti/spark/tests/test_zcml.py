8cf03c0c1851bf35ef1bd848e7894b3740934ce4
Carlos Sanchez <papachoco@gmail.com>
2018-01-09 16:52:21 -0600

    only verify registrations

--- src/nti/spark/tests/test_zcml.py
+++ src/nti/spark/tests/test_zcml.py
@@ -7,16 +7,12 @@ from __future__ import absolute_import
 
 # pylint: disable=protected-access,too-many-public-methods
 
+from hamcrest import none
+from hamcrest import is_not
 from hamcrest import assert_that
-
-from nti.testing.matchers import validly_provides
-from nti.testing.matchers import verifiably_provides
 
 from zope import component
 
-from nti.spark.interfaces import IHiveContext
-from nti.spark.interfaces import ISparkContext
-from nti.spark.interfaces import ISparkSession
 from nti.spark.interfaces import ISparkInstance
 from nti.spark.interfaces import IHiveSparkInstance
 
@@ -33,7 +29,7 @@ SPARK_ZCML_STRING = u"""
 
     <include package="." file="meta.zcml" />
 
-    <spark:registerSparkInstance/>
+    <spark:registerSparkInstance app_name="MainSpark App" />
 
 </configure>
 """
@@ -59,19 +55,12 @@ class TestZcml(nti.testing.base.ConfiguringTestBase):
 
     def test_spark_registration(self):
         self.configure_string(SPARK_ZCML_STRING)
-        spark = component.getUtility(ISparkInstance)
-        assert_that(spark, validly_provides(ISparkInstance))
-        assert_that(spark, verifiably_provides(ISparkInstance))
-        assert_that(spark.context, validly_provides(ISparkContext))
-        assert_that(spark.context, verifiably_provides(ISparkContext))
-        assert_that(spark.session, validly_provides(ISparkSession))
-        assert_that(spark.session, verifiably_provides(ISparkSession))
+        spark = component.queryUtility(ISparkInstance)
+        assert_that(spark, is_not(none()))
         spark.close()
 
     def test_hive_spark_registration(self):
         self.configure_string(HIVESPARK_ZCML_STRING)
-        spark = component.getUtility(IHiveSparkInstance)
-        assert_that(spark, validly_provides(IHiveSparkInstance))
-        assert_that(spark, verifiably_provides(IHiveSparkInstance))
-        assert_that(spark.hive, validly_provides(IHiveContext))
+        spark = component.queryUtility(IHiveSparkInstance)
+        assert_that(spark, is_not(none()))
         spark.close()
