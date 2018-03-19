9527a820a455ec20f4db528ec86f3b850272fe0f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-19 13:41:10 -0500

    add mixin

--- src/nti/spark/scripts/tests/__init__.py
+++ src/nti/spark/scripts/tests/__init__.py
@@ -4,3 +4,17 @@
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
+
+import os
+import shutil
+
+
+class BaseTestMixin(object):
+
+    @classmethod
+    def clean_up(cls):
+        shutil.rmtree(os.path.join(os.getcwd(), 'metastore_db'), True)
+        shutil.rmtree(os.path.join(os.getcwd(), 'spark-warehouse'), True)
+
+    def tearDown(self):
+        self.clean_up()
